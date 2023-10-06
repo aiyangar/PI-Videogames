e4d1fb8e7b09ab8536bbfa9d4f018f1a2a6a7068
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-06 16:02:12 -0600

    Inicio PI

---
+++ api/src/routes/gamesRouter.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const gamesRouter = Router();
+
+module.exports = gamesRouter;
---
+++ api/src/routes/mainRouter.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+
+const gamesRouter = require('./gamesRouter');
+const genresRouter = require('./genresRouter');
+
+
+const router = Router();
+
+router.use('/videogames', gamesRouter);
+router.use('/genres', genresRouter);
+
+module.exports = router;
