e7d23959836fdd292529963efe03c612c060080a
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-09 09:07:19 -0600

    terminado backend

--- api/src/controllers/gamesControllers.js
+++ api/src/controllers/gamesControllers.js
@@ -22,37 +22,42 @@ const getGameByName = async (name) => {
         },
         limit: 15
     });
-    const infoApi = (await axios.get(`${URL}/games?search=${name}&key=${KEY}&pageSize=15`)).data.results;
+    const infoApi = (
+        await axios.get(`${URL}/games?search=${name}&key=${KEY}&pageSize=15`)
+    ).data.results;
     const gamesApi = cleanArray(infoApi);
-    const gamesApiFiltered = gamesApi.filter((game) => game.name.toLowerCase().includes(name.toLowerCase()));
+    const gamesApiFiltered = gamesApi.filter((game) =>
+        game.name.toLowerCase().includes(name.toLowerCase())
+    );
+
     const result = [...gamesDB, ...gamesApiFiltered];
+
     if (result.length === 0) {
         return { message: `No se encontró ningún videojuego que coincida con: '${name}'.` };
     }
+
     return result.slice(0, 15);
 };
 
-const getGameByID = async(id, source) => {
-    console.log(URL)
-    console.log(KEY)
+
+const getGameByID = async (id, source) => {
     const response =
         source === 'api'
-        ? (await axios.get(`${URL}/games/${id}?key=${KEY}`))
-        : (await Videogame.findByPk(id));
+            ? await axios.get(`${URL}/games/${id}?key=${KEY}`)
+            : await Videogame.findByPk(id);
+    
+        if (source === 'api') {
+        const { id, name, description, released, platforms, background_image, rating, genres, } = response.data;
+    
+        const platformsName = platforms.map((data) => data.platform.name);
+        const genresName = genres.map((data) => data.name);
+    
+        return cleanData({ id, name, description, released, platforms: platformsName, background_image, rating, genres: genresName, });
+    } else {
+        return response;
+    }
+};
 
-        if (source === "api") {
-            const { id,name, description, released, platforms,background_image,rating,genres} = response.data;
-
-            const platformsName = platforms.map(data => data.platform.name);
-            genresName = genres.map(data=>data.name); 
-
-            const cleanedData = cleanData({id, name, description, released, platforms: platformsName,background_image,rating,genres: genresName})
-
-            return cleanedData;
-        } else {
-            return response;
-        }
-}
 
 const createGameDB = async(key,name, description, platforms, image, releaseDate, rating, genre, created) => {
 
