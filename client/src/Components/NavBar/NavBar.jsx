5997a9d91e78e117b2a02a1370bce126b3d525cf
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-23 11:12:13 -0600

    Cambié el menú de etiquetas 'a' a etiquetas Link

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import videogamesLogo from './Logo.svg'
-import './NavBar.styles.css'
-import SearchBar from '../SearchBar/SearchBar'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importa Link
+import videogamesLogo from './Logo.svg';
+import './NavBar.styles.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 const NavBar = () => {
-  
   return (
     <div className='navbarContainer'>
-      <a href='/' className='logo'><img src={videogamesLogo} alt="Logo" /></a>
+      <Link to="/" className='logo'>
+        <img src={videogamesLogo} alt="Logo" />
+      </Link>
       <nav className="mainMenuContainer">
         <ul className="mainMenu">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/videogames">Videogames</a></li>
-          <li><a href="/create">Create</a></li>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/videogames">Videogames</Link></li>
+          <li><Link to="/create">Create</Link></li>
         </ul>
       </nav>
       <SearchBar />
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
