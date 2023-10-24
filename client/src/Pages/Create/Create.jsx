2327e356dc00fbf03e77cd905d9cec022a1dc8aa
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-24 14:32:42 -0600

    se retiró la validación y se modificó el modelo

--- client/src/Pages/Create/Create.jsx
+++ client/src/Pages/Create/Create.jsx
@@ -44,8 +44,7 @@ const create = () => {
   const platform = allPlatforms.map(platform => platform.name)
 
   const validate = (state, name) => {
-    const dateRegex = /^(((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02])|(0[1-9]|[12][0-9]|30)[- /.](0[469]|11)|(0[1-9]|1\d|2[0-8])[- /.]02)[- /.]\d{4}|29[- /.]02[- /.](\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$/;
-
+    const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
     if (name === 'name'){
       if (state.name === ''){
@@ -89,8 +88,8 @@ const create = () => {
     if (name === 'released'){
       if (state.released === ''){
         setErrors({...errors, released: 'Selecciona una fecha'})
-      } else if (!dateRegex.test(state.released)) {
-        setErrors({...errors, released: 'Selecciona una fecha válida'})
+      // } else if (!dateRegex.test(state.released)) {
+      //   setErrors({...errors, released: 'Selecciona una fecha válida'})
       } else {
         setErrors({...errors, released: ''})
       }
