e35d9982774d43ef5445070acca8242333b9d3e6
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-23 21:21:07 -0600

    terminada la landing

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Landing.styles.css';
-import videogameImage from './videogame.png';
+import image1 from './Videogame1.png';
+import image2 from './Videogame2.png';
+import image3 from './Videogame3.png';
+import image4 from './Videogame4.png';
 
 const Landing = () => {
   return (
     <div>
       <div className="mainContainer">
-        <h1>PI - Videogames</h1>
-        <div className="principalContent">
-          <img src={videogameImage} alt="videogameImage" />
-          <Link to="/videogames">
-            <button>Comenzar</button>
+        <div className="mainSlider">
+          <Link to={'/videogames'}>
+            <ul>
+              <li><img src={image1} alt="" /></li>
+              <li><img src={image2} alt="" /></li>
+              <li><img src={image3} alt="" /></li>
+              <li><img src={image4} alt="" /></li>
+            </ul>
           </Link>
         </div>
-        
       </div>
     </div>
   );
