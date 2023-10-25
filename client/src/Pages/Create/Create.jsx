58f58cb23d809581379d4c68ed916c10cf8f9267
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-25 11:16:24 -0600

    comentada la línea del dateRegex

--- client/src/Pages/Create/Create.jsx
+++ client/src/Pages/Create/Create.jsx
@@ -44,7 +44,7 @@ const create = () => {
   const platform = allPlatforms.map(platform => platform.name)
 
   const validate = (state, name) => {
-    const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    // const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
     if (name === 'name'){
       if (state.name === ''){
