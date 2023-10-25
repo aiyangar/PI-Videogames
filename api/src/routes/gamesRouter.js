48a4f573d4eafc45e32ac41f9fc4373d43d0cdf3
aiyangar <ing.gustavo.cardenas@gmail.com>
2023-10-25 16:21:28 -0600

    quité un espacio de routes

--- api/src/routes/gamesRouter.js
+++ api/src/routes/gamesRouter.js
@@ -7,9 +7,8 @@ const { getGamesHandler,
   createGameHandler
 } = require('../handlers/gamesHandlers');
 
-gamesRouter.get('/', getGamesHandler); //videogames/?name:chuchita
-gamesRouter.get('/:id', getDetailHandler); //videogames/1
-
+gamesRouter.get('/', getGamesHandler);
+gamesRouter.get('/:id', getDetailHandler);
 gamesRouter.post('/', createGameHandler);
 
 module.exports = gamesRouter;
